7cbcc84e42a6f34cd32430698228591b155e3062
Midhun Mohan <k.midhun@outlook.com>
2023-03-31 01:05:12 -0400

    adhoc updated

--- airflow_ge/dags/ad_hoc.py
+++ airflow_ge/dags/ad_hoc.py
@@ -9,13 +9,13 @@ from airflow.models.param import Param
 from datetime import timedelta, datetime
 import snowflake.connector
 
-openai.api_key = "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"
+openai.api_key = ""
 
 
 s3 = boto3.client(
     's3',
-    aws_access_key_id="AKIAZW4EPXNK5AQIEQWO",
-    aws_secret_access_key="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",
+    aws_access_key_id="",
+    aws_secret_access_key="",
 )
 
 
